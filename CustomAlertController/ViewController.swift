f17c18732765f9460ea5cac02b66b4c9d4303786
beakchild <crt2112@gmail.com>
2018-10-25 11:15:23 +0900

    Initial Commit

---
+++ CustomAlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAlertController
+//
+//  Created by beakhand on 2018/10/25.
+//  Copyright © 2018年 beakhand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
